92c076a584e255573328206ce941598958ff84fd
Whc <wanghancheng219@gmail.com>
2024-04-09 17:57:35 +0100

    plotaudio test

---
+++ src/tests/plotaudio/plotaudio.h
@@ -0,0 +1,16 @@
+#ifndef PLOTAUDIO_H
+#define PLOTAUDIO_H
+
+#include <iostream>
+#include <vector>
+#include <sndfile.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+void plotAudio(const std::string& audioFileName);
+
+#endif // PLOTAUDIO_H
