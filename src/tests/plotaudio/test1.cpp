b10fcf08edb25c5a138c8ee7bb4a89438d4fc6e9
Whc <wanghancheng219@gmail.com>
2024-04-09 17:54:46 +0100

    test1.cpp

---
+++ src/tests/plotaudio/test1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "plotaudio.h"
+
+int main(){
+    std::string fileName = "/home/whc/Desktop/bicycle_bell.wav";
+    plotAudio(fileName);
+    return 0;
+}
